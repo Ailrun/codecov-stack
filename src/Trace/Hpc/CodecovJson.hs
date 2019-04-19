54b247e5aa11f7d84c6c8c69f24a318183a104b2
Ailrun <jjc9310@gmail.com>
2019-04-19 04:55:13 -0400

    Add interfaces for CodecovJson data type

--- src/Trace/Hpc/CodecovJson.hs
+++ src/Trace/Hpc/CodecovJson.hs
@@ -1,3 +1,14 @@
 module Trace.Hpc.CodecovJson where
 
+import Trace.Hpc.Mix
+import Trace.Hpc.Tix
+
+import qualified Data.ByteString.Lazy as BSL
+
 data CodecovJson
+
+fromTixAndMixes :: Tix -> [Mix] -> CodecovJson
+fromTixAndMixes tix mixes = undefined
+
+toLBS :: CodecovJson -> BSL.ByteString
+toLBS ccJson = undefined
